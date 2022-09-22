888ce2cb6a58ccd6166d9c974951f17075cc2e16
rebecca-2022 <rebecca.njeri@student.moringaschool.com>
2022-09-22 11:32:02 +0300

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -2,7 +2,8 @@ import React from "react";
 import howManyParks from "./parks/howManyParks";
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  howManyParks(); 
+  
 
   return <h1>Colorado State Parks!</h1>;
 }
